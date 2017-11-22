15b3347fe5b6fac6f6e898d792456f3300e1afe3
Paul Zavalza <zse.paul@gmail.com>
2017-11-21 22:01:05 -0600

    user can see details of pokemon

--- pokedex.js
+++ pokedex.js
@@ -6,17 +6,22 @@ Pokedex = {
     this.pokemons = pokemons;
   },
 
-  displayPokemon: function(){
-    document.getElementById("pokemonImage").src = this.pokemons[this.index].img;
-    document.getElementById("pokemonName").innerHTML = this.pokemons[this.index].name;
-    document.getElementById("pokemonNumber").value = this.pokemons[this.index].num;
+  showPokemonCard: function(){
+    this._buildPokemon();
+    document.getElementById("pokemonDetails").style.display = "none";
+    document.getElementById("pokemonCard").style.display = "block";
+  },
+
+  showPokemonDetails: function(){
+    document.getElementById("pokemonCard").style.display = "none";
+    document.getElementById("pokemonDetails").style.display = "block";
   },
 
   setIndex: function(pokemonNumber){
     if(pokemonNumber > 0 && pokemonNumber <= 151) {
       this.index = pokemonNumber -1;
     }
-    this.displayPokemon();
+    this.showPokemonCard();
   },
 
   decrementIndex : function(){
@@ -27,7 +32,7 @@ Pokedex = {
       this.index = this.index-1;
     }
 
-    this.displayPokemon();
+    this.showPokemonCard();
   },
 
   incrementIndex : function(){
@@ -37,6 +42,24 @@ Pokedex = {
     else {
       this.index = 0;
     }
-    this.displayPokemon();
+    this.showPokemonCard();
+  },
+
+  _buildPokemon: function(){
+    let pokemon = this.pokemons[this.index];
+    document.getElementById("pokemonImage").src = pokemon.img;
+    document.getElementById("pokemonName").innerHTML = pokemon.name;
+    document.getElementById("pokemonNumber").value = pokemon.num;
+    document.getElementById("pokemonType").innerHTML = "Type: " + pokemon.type;
+    document.getElementById("pokemonHeight").innerHTML = "Height: " + pokemon.height;
+    document.getElementById("pokemonWeight").innerHTML = "Weight: " + pokemon.weight;
+    document.getElementById("pokemonCandy").innerHTML = "Candy: " + pokemon.candy;
+    document.getElementById("pokemonCandyCount").innerHTML = "Candy count: " + pokemon.candy_count;
+    document.getElementById("pokemonEgg").innerHTML = "Egg: " + pokemon.egg;
+    document.getElementById("pokemonSpawnChance").innerHTML = "Spawn chance: " + pokemon.spawn_chance;
+    document.getElementById("pokemonAvgSpawns").innerHTML = "Avg spawns: " + pokemon.avg_spawns;
+    document.getElementById("pokemonSpawnTime").innerHTML = "Spawn time: " + pokemon.spawn_time;
+    document.getElementById("pokemonMultipliers").innerHTML = "Multipliers: " + pokemon.multipliers;
+    document.getElementById("pokemonWeaknesses").innerHTML = "Weaknesses: " + pokemon.weaknesses;
   }
 }
